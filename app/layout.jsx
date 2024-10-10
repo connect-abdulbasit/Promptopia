a29558a8170373029991f39ec3ca1d878e45fc8d
Abdul Basit <connect.abdulbasit.cs@gmail.com>
2024-10-11 00:51:23 +0500

    Promptopia basic structure and half of the nav work is completed

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+export const metadata = {
+  title: "Promptopia",
+  description: "Dscover & share AI prompts",
+};
+const RouteLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RouteLayout;
