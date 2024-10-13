6de7a0a3ca47593b9574c79868259c0da035cc44
Abdul Basit <connect.abdulbasit.cs@gmail.com>
2024-10-13 21:14:30 +0500

    Remove saving in cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,16 +1,29 @@
-import { connectToDB } from "@utils/database";
-import Prompt from "@models/prompt";
-
 export const GET = async (req) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
+
     return new Response(JSON.stringify(prompts), {
       status: 200,
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+        Pragma: "no-cache",
+        Expires: "0",
+        "Surrogate-Control": "no-store",
+      },
     });
   } catch (error) {
+    console.error("Error fetching prompts:", error);
     return new Response("Failed to fetch all prompts", {
       status: 500,
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+        Pragma: "no-cache",
+        Expires: "0",
+        "Surrogate-Control": "no-store",
+      },
     });
   }
 };
