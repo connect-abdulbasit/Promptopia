ba9602fc747be674b1874860c1f28871469962eb
Abdul Basit <connect.abdulbasit.cs@gmail.com>
2024-10-14 14:21:15 +0500

    Cache remove

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -10,7 +10,7 @@ export const GET = async (req) => {
       status: 200,
       headers: {
         "Cache-Control":
-          "no-store, no-cache, must-revalidate, proxy-revalidate",
+          "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0, s-maxage=0",
         Pragma: "no-cache",
         Expires: "0",
         "Surrogate-Control": "no-store",
